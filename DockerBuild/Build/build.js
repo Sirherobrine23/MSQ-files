0109d1b8d21de579faf34eec760e94a0aec78b41
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-06-01 23:10:00 -0300

    Test

--- DockerBuild/Build/build.js
+++ DockerBuild/Build/build.js
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 const { resolve } = require("path");
+const { exit } = require("process");
+const { arch } = process.env
 
 // Setting Docker Build
-var load_config = {
+const load_config = {
     image: "dockerruns"
 }
-const { arch } = process.env
-console.log(load_config.arch);
 const command = `docker buildx build --file Dockerfile --platform=${arch} -t ${load_config.image.toLocaleLowerCase()} .`
 console.log(command);
-const build = exec(command, {cwd: resolve(__dirname), maxBuffer: Infinity});
+const build = exec(command, {cwd: resolve(__dirname, "../"), maxBuffer: Infinity});
 function out(data){
     data = `${data}`.split("\n").filter(data=>{return (data !== "")})
     for (let log of data) console.log(`${arch}: ${log}`);
 }
 build.stdout.on("data", data => out(data));
 build.stderr.on("data", data => out(data));
-build.on("exit", code => {if (code !== 0) process.exit(code)});
+build.on("exit", code => {
+    if (code !== 0) exit(code);
+    else {
+        const DockerSHADeCamomila = JSON.parse(execSync(`docker manifest inspect -v dockerruns`).toString())[0].Descriptor.digest
+        const command = `docker run -v ${resolve(__dirname, "../")}:/home/OUT/ dockerruns@${DockerSHADeCamomila}`
+        const getfile = exec(command, {cwd: resolve(__dirname, "../"), maxBuffer: Infinity});
+        getfile.stdout.on("data", data => out(data))
+        getfile.stderr.on("data", data => out(data))
+        getfile.on("exit", cde => exit(cde))
+    }
+});
