e9ea6f54ae889eaaaa054d1b4535f30ad97e5b6d
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-06-01 22:51:25 -0300

    TEst

--- DockerBuild/Build/build.js
+++ DockerBuild/Build/build.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-const { exec, execSync } = require("child_process");
-const { existsSync, writeFileSync, readFileSync } = require("fs");
+const { exec } = require("child_process");
 const { resolve } = require("path");
 
 // Setting Docker Build
 var load_config = {
     image: "dockerruns"
 }
-
+const { arch } = process.env
 console.log(load_config.arch);
-const command = `docker buildx build --file Dockerfile --platform=${process.env.arch} -t ${load_config.image.toLocaleLowerCase()} .`
+const command = `docker buildx build --file Dockerfile --platform=${arch} -t ${load_config.image.toLocaleLowerCase()} .`
 console.log(command);
 const build = exec(command, {cwd: resolve(__dirname), maxBuffer: Infinity});
 function out(data){
