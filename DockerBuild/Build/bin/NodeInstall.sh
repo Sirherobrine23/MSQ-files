00694d334f4a1ff58cee75f252fdff2ea7cf4b10
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-06-01 23:24:01 -0300

    Test

--- DockerBuild/Build/bin/NodeInstall.sh
+++ DockerBuild/Build/bin/NodeInstall.sh
@@ -4,9 +4,9 @@ cd /tmp
 tag=$(wget -qO- https://api.github.com/repos/nodejs/node/releases | jq -r ".[0].tag_name")
 
 copilerNode(){
-    set -ex
     cd /tmp
-    echo "git clone https://github.com/nodejs/node.git --depth=1 --branch $1" | bash -
+    mkdir node
+    wget -qO- "https://nodejs.org/download/release/${tag}/node-${tag}.tar.gz" | tar xfz - --directory node/
     cd node
     ./configure
     make -j$(nproc)
@@ -20,10 +20,10 @@ case $(uname -m) in
     x86_64)  url="https://nodejs.org/download/release/${tag}/node-${tag}-linux-x64.tar.gz";;
     aarch64) url="https://nodejs.org/download/release/${tag}/node-${tag}-linux-arm64.tar.gz";;
     arm64)   url="https://nodejs.org/download/release/${tag}/node-${tag}-linux-arm64.tar.gz";;
-    armv7l)  copilerNode $tag ;;
+    armv7l)  url="https://nodejs.org/download/release/${tag}/node-${tag}-linux-armv7l.tar.gz";;
     ppc64le) url="https://nodejs.org/download/release/${tag}/node-${tag}-linux-ppc64le.tar.gz";;
     s390x)   url="https://nodejs.org/download/release/${tag}/node-${tag}-linux-s390x.tar.gz";;
-    *) copilerNode $tag ;;
+    *) copilerNode;;
 esac
 
 if [ ! -z "${url}" ];then
