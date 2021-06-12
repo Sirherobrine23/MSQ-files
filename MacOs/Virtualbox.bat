295d042884391a557945c007f65e61e57bd1c33e
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-06-12 02:36:19 +0000

    Create Virtualbox.bat

---
+++ MacOs/Virtualbox.bat
@@ -0,0 +1,8 @@
+cd "C:\Program Files\Oracle\VirtualBox\"
+VBoxManage.exe modifyvm "MacOS" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+VBoxManage setextradata "MacOS" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMacOS11,3"
+VBoxManage setextradata "MacOS" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+VBoxManage setextradata "MacOS" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
+VBoxManage setextradata "MacOS" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+VBoxManage setextradata "MacOS" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+VBoxManage setextradata "MacOS" "VBoxInternal2/EfiGraphicsResolution" "1360x768"
